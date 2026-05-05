91fa871c829e7ccd71ee33549098906e1ff227a7
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-05-05 15:42:15 +0530

    create profit graph using scatter

--- python_aiml_topic.py/movie_dataset.py
+++ python_aiml_topic.py/movie_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('hollywood_movies_sample.csv')
 print(df)
@@ -11,3 +12,13 @@ df["success"] = df["profit_million"].apply(
 )
 
 print(df[["title", "profit_million", "success"]])
+
+# Visulaize budget vs revinue
+
+plt.scatter(df['budget_million'], df['revenue_million'])
+plt.xlabel("Budget in Millian Doller")
+plt.ylabel('Revinue in Millian Dollar')
+plt.title("Budget vs Revenue")
+plt.show()
+
+
