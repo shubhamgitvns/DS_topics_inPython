ecc71161a457a2a95936af3f901edf034a12ba06
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-05 17:31:26 +0530

    explore the data set using pandas

--- python_aiml_topic.py/ml_toppics/classification/projects/houserent_classification.py
+++ python_aiml_topic.py/ml_toppics/classification/projects/houserent_classification.py
@@ -1,23 +1,23 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
 
+# loading the data from sckit lern datset
 housing = fetch_california_housing()
 
 # print all about the data
-
 # print(housing)
-print("Keys:")
-print(housing.keys())
-print("Data:")
-print(housing.data)
-print("Target:")
-print(housing.target)
-print("Frame:")
-print(housing.frame)
-print("Target_name:")
-print(housing.target_names)
-print("Feature_name:")
-print(housing.feature_names)
-print("Descr:")
-print(housing.DESCR)
+
+# create the data frame for exploring data
+df = pd.DataFrame(
+    housing.data,
+   columns= housing.feature_names
+)
+# create the price column on dataset
+df['Price']= housing.target
+
+# descrive the data or understand the datsets 
+print(df.head())
+print(df.info)
+print(df.describe())
+print(df.isnull().sum())
 
