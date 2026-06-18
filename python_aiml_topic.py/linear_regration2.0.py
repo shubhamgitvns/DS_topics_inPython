a82d0592da9d0b36a96e95b37edde0ee3151b6dd
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-18 23:07:51 +0530

    convert the liner regression code in function

--- python_aiml_topic.py/linear_regration2.0.py
+++ python_aiml_topic.py/linear_regration2.0.py
@@ -1,39 +1,52 @@
+import matplotlib.pyplot as plt
+def linear_prediction(x, y, X):
 
-x=[8, 10, 12]
-y=[10, 13, 16]
-n = len(x)
+    n = len(x)
 
-mean_X= sum(x)/n
-mean_Y= sum(y)/n
-div_X = []
-div_Y = []
-p_of_d =[]
-sqr_of_div_X = []
+    mean_X = sum(x) / n
+    mean_Y = sum(y) / n
+
+    div_X = []
+    div_Y = []
+
+    sum_of_sqr_of_div_X = 0
+    sum_of_pd = 0
+
+    for i in range(n):
+
+        div_X.append(x[i] - mean_X)
+
+        div_Y.append(y[i] - mean_Y)
+
+        sum_of_pd += div_X[i] * div_Y[i]
+
+        sum_of_sqr_of_div_X += div_X[i] * div_X[i]
+
+    # linear regression: Y = mX + b
+
+    m = sum_of_pd / sum_of_sqr_of_div_X
+
+    b = mean_Y - (m * mean_X)
+
+    prediction = m * X + b
+
+    return prediction
 
 
-for i in range(n):
-    
-    div_X.append(x[i] - mean_X)
+x = [8, 10, 12]
+y = [10, 13, 16]
 
-    div_Y.append(y[i] - mean_Y)
+ans = linear_prediction(x, y, 20)
 
-    p_of_d.append(div_X[i] * div_Y[i]) 
-    sqr_of_div_X.append(div_X[i] * div_X[i])
+print("Prediction =", ans)
 
-sum_of_pd = sum(p_of_d)
-sum_of_sqr_of_div_X = sum(sqr_of_div_X)
+# Create the graph
 
+# plt.scatter(x, y, color= "green")
+# plt.scatter(X,prediction, color = "red")
+# plt.xlabel("Pizza in (inches)")
+# plt.ylabel("Price of Pizza")
+# plt.title("Pizza price prdiction")
+# plt.show()
 
-
-# linear regration = Y=mX+b
-# slop/cooficent m = sum of product division / sum of sqt of division
-# b = mean of y - (m* mean of x)
-
-m = sum_of_pd / sum_of_sqr_of_div_X
-b= mean_Y - (m * mean_X)
-
-#prediction
-X = 20
-y = m * X + b
-print("prediction", y)
     
